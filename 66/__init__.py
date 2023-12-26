d73c6be9d8ea816bb76767233597f1cf3c9b632e
belsssskaya <kate39825@gmail.com>
2023-12-26 23:03:32 +0700

    меню

--- 66/__init__.py
+++ 66/__init__.py
@@ -57,11 +57,35 @@ def main ():
         print("4. Завершить работу программы")
         choice = int(input("Введите номер выбранного пункта: "))
         if choice == 1:
-            pass
+            journal_name = input("Введите название журнала: ")
+            journal = Journal(journal_name)
+            author_names = input("Введите имена авторов через запятую: ").split(", ")
+            for author_name in author_names:
+                author = Author(author_name)
+                journal.add_author(author)
+                publications = input(
+                    "Введите публикации в формате 'название, год' (каждая публикация с новой строки, пустая строка для завершения): ")
+            while publications:
+                title, year = publications.split(", ")
+                publication = Publication(title, year, author)
+                journal.add_publication(publication)
+                publications = input()
+            journals.append(journal)
         elif choice == 2:
-            pass
+            print("Содержимое журналов:")
+            for journal in journals:
+                journal.display_contents()
         elif choice == 3:
-            pass
+            journal_name = input("Введите название журнала: ")
+            selected_journal = None
+            for journal in journals:
+                if journal.name == journal_name:
+                    selected_journal = journal
+                    break
+            if selected_journal:
+                selected_journal.display_representation()
+            else:
+                print("Журнал не найден")
         elif choice == 4:
             print("Программа завершена.")
             break
